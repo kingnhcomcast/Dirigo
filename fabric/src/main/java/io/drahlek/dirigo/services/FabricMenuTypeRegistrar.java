eabc5aa2d38c01191dbaeac5fa412e928cfa8518
Kevin King <kingnh@comcast.net>
2026-05-14 20:08:09 -0400

    fix build

--- fabric/src/main/java/io/drahlek/dirigo/services/FabricMenuTypeRegistrar.java
+++ fabric/src/main/java/io/drahlek/dirigo/services/FabricMenuTypeRegistrar.java
@@ -4,13 +4,10 @@ import io.drahlek.dirigo.services.services.IMenuTypeRegistrar;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
-import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Proxy;
 import java.util.function.Supplier;
 
 public class FabricMenuTypeRegistrar implements IMenuTypeRegistrar {
@@ -28,34 +25,7 @@ public class FabricMenuTypeRegistrar implements IMenuTypeRegistrar {
         return () -> type;
     }
 
-    @SuppressWarnings("unchecked")
     private static <T extends AbstractContainerMenu> MenuType<T> createType(MenuFactory<T> factory) {
-        try {
-            Class<?> supplierClass = Class.forName("net.minecraft.world.inventory.MenuType$MenuSupplier");
-            Object supplier = Proxy.newProxyInstance(
-                    MenuType.class.getClassLoader(),
-                    new Class<?>[]{supplierClass},
-                    (proxy, method, args) -> {
-                        if ("create".equals(method.getName())) {
-                            return factory.create((int) args[0], (Inventory) args[1]);
-                        }
-                        if ("toString".equals(method.getName())) {
-                            return factory.toString();
-                        }
-                        if ("hashCode".equals(method.getName())) {
-                            return System.identityHashCode(proxy);
-                        }
-                        if ("equals".equals(method.getName())) {
-                            return proxy == args[0];
-                        }
-                        throw new UnsupportedOperationException(method.toString());
-                    }
-            );
-            Constructor<?> constructor = MenuType.class.getDeclaredConstructor(supplierClass, FeatureFlagSet.class);
-            constructor.setAccessible(true);
-            return (MenuType<T>) constructor.newInstance(supplier, FeatureFlagSet.of());
-        } catch (ReflectiveOperationException e) {
-            throw new IllegalStateException("Failed to create menu type", e);
-        }
+        return new MenuType<>(factory::create, FeatureFlagSet.of());
     }
 }
